e728caab2532934aa540f399f55c65babb6439d3
holgerschurig <holgerschurig@8f11f118-d7e1-0310-9c17-a614ba8dcf8f>
2005-01-08 21:25:02 +0000

    The idea here is to be able to dial-by-name.

---
+++ trunk/cfg_app_directory.py
@@ -0,0 +1,37 @@
+# -*- coding: iso-latin-1 -*-
+#
+# Copyright (C) 2004 by Holger Schurig
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+
+from configlets import *
+
+
+class CfgAppDirectory(CfgApp):
+
+	shortName   = _("Directory")
+	description = _("""The application allows you to dial by name.""")
+	variables   = [VarType("ext", title=_("Extension"), len=6),
+		       VarType("vmcontext", title=_("Voicemail Context"), default="default", len=15),
+		       VarType("context", title=_("Context"), len=15)]
+
+	def createAsteriskConfiglet(self):
+		c = AstConf("extensions.conf")
+		c.setSection(self.context)
+		c.appendExten(self.ext, "Directory(%s)" % self.vmcontext)
+		c.appendExten(self.ext, "Hangup")
+
