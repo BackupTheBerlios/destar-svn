7778f56a893ad19ead9975369e02e4381e79c754
holgerschurig <holgerschurig@8f11f118-d7e1-0310-9c17-a614ba8dcf8f>
2005-04-30 19:53:00 +0000

    make incoming ZAP channel work

--- trunk/cfg_trunk_zap.py
+++ trunk/cfg_trunk_zap.py
@@ -29,24 +29,39 @@ class CfgTrunkZap(CfgTrunk):
 		VarType("channel",    title=_("Zaptel channel number"), type="string", len=5),
 		VarType("signalling", title=_("Signalling type"), type="choice",
 		                      options=[('fxs_ls','loopstart'),('fxs_ks', 'kewlstart')]),
-		VarType("group",      title=_("Callout group"), type="int", default=1),
 
 		VarType("Outbound",   title=_("Calls to the PSTN network"), type="label"),
+		VarType("group",      title=_("Callout group"), type="int", default=1),
 		VarType("ext",        title=_("Outgoing prefix"), optional=True, len=6),
+		VarType("context",    title=_("Context"), default="default", optional=True, hide=True),
+
+		VarType("Inbound",    title=_("Incoming calls"), type="label"),
+		VarType("phone",      title=_("Phone to ring"), optional=True, type="choice",
+		                               options=getChoice("CfgPhone")),
+		VarType("contextin",  title=_("Context"), optional=True, hide=True, default="in-zaptrunk")
 		]
 
 
 	def fixup(self):
 		CfgTrunk.fixup(self)
-		#useContext("in-pstn")
+		useContext(self.context)
+		useContext(self.contextin)
 
 
 	def createAsteriskConfig(self):
 		needModule("chan_zap")
 
+		c = AstConf("zaptel.conf")
+		c.setSection("")
+		c.destar_comment = False
+		c.append("fxs%s=%s" % (self.signalling[4:], self.channel))
+		c.append("")
+
 		# Create config for chan_zap:
 		c = AstConf("zapata.conf")
-		c.append("context=default")
+		c.append("")
+		c.append("; Zaptel Trunk %s" % self.name)
+		c.append("context=%s" % self.contextin)
 		c.appendValue(self, "signalling")
 		c.appendValue(self, "group")
 		c.appendValue(self, "channel")
@@ -55,13 +70,13 @@ class CfgTrunkZap(CfgTrunk):
 		# Write special dialout entry
 		# TODO: we should not have ONE dialout entry, but several of them,
 		# e.g. for local calls, national calls, foreign calls etc
+		c = AstConf("extensions.conf")
 		if self.ext:
-			c = AstConf("extensions.conf")
-			c.setSection("default")
+			c.setSection(self.context)
 			c.appendExten("%s" % self.ext, "Dial(Zap/%s/${EXTEN:%d},60,T)" % (self.channel,len(self.ext)))
 
-		c = AstConf("zaptel.conf")
-		c.setSection("")
-		c.destar_comment = False
-		c.append("fxs%s=%s" % (self.signalling[4:], self.channel))
-		c.append("")
+		if self.phone and self.contextin:
+			c.setSection(self.contextin)
+			c.appendExten("s", "Goto(default,%s,1)" % self.phone)
+
+
