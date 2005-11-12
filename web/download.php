a8cd491b2ca44c60770dba01c874296c24d90259
alerios <alerios@8f11f118-d7e1-0310-9c17-a614ba8dcf8f>
2005-11-12 17:09:50 +0000

    * Improved resources.php

--- web/download.php
+++ web/download.php
@@ -1,30 +1,31 @@
 <h2 id="contentHeading">Download Area</h2>
 
-<p>To see the <b>Software Requirements</b> of destar, please look at the 
+<p>To see the <b>Software Requirements</b> of DeStar, please look at the 
 <a href="http://openfacts.berlios.de/index-en.phtml?title=DeStarInstall">Installation Instructions</a>.<p>
 
 <h3>Get Destar v0.1, for Asterisk 1.0.X</h3>
 
 <h4>Tarball (.tgz) file</h4>
 
-<p><a href="ftp://ftp.berlios.de/pub/destar/destar-0.1.tar.gz">destar-0.1.tar.gz</a>/p>
-<p><a href="ftp://ftp.berlios.de/pub/destar/MD5SUMS">MD5SUMS</a>/p>
+<ul>
+<li><a href="ftp://ftp.berlios.de/pub/destar/destar-0.1.tar.gz">destar-0.1.tar.gz</a></li>
+<li><a href="ftp://ftp.berlios.de/pub/destar/MD5SUMS">MD5SUMS</a></li>
+</ul>
 
 <h4>Debian package</h4>
 
-<p>On testing or unstable:  #apt-get install destar</p>
-
-<p><a href="http://packages.debian.org/cgi-bin/search_packages.pl?searchon=names&subword=1&version=all&release=all&keywords=destar&sourceid=mozilla-search">Deb file</a>./p>
+<ul>
+<li>On testing or unstable:  <i>#apt-get install destar</i></li>
+<li><a href="http://packages.debian.org/cgi-bin/search_packages.pl?searchon=names&subword=1&version=all&release=all&keywords=destar&sourceid=mozilla-search">Deb file</a>.</li>
+</ul>
 
 <h4>Latest svn snapshot</h4>
 
 <p>To grab the last svn copy:</p>
 
-Source code for read-only access:
+<p><i>svn checkout svn://svn.berlios.de/destar/trunk</i></p>
 
-* svn checkout svn://svn.berlios.de/destar/trunk
+<p>Web access to Subversion repository:</p>
 
-Web access to Subversion repository:
-
-* http://svn.berlios.de/viewcvs/destar/trunk/
+<p><i>http://svn.berlios.de/viewcvs/destar/trunk/</i></p>
 
--- web/resources.php
+++ web/resources.php
@@ -1,24 +1,36 @@
 <h2 id="contentHeading">Resources & Support</h2>
 
-Users mailing list:
+<h3>Users mailing list:</h3>
 
-* http://lists.berlios.de/mailman/listinfo/destar-user
-* destar-user@lists.berlios.de
-
-IRC channel:
-
-* #destar at irc.freenode.net
-
-Bug tracking system:
+<ul>
+<li><a href="http://lists.berlios.de/mailman/listinfo/destar-user">List Info (archives, subscriptions)</a>.</li>
+<li>destar-user@lists.berlios.de</li>
+</ul>
 
 
-Project Wiki:
+<h3>IRC channel:</h3>
 
-* http://openfacts.berlios.de/index-en.phtml?title=DeStar
+<p>#destar at irc.freenode.net</p>
 
 
-Developers mailing list:
+<h3>Bug tracking system:</h3>
 
-* http://lists.berlios.de/mailman/listinfo/destar-dev
-* destar-dev@lists.berlios.de
+<ul>
+<li><a href="http://developer.berlios.de/bugs/?func=addbug&group_id=2112">Submit a Bug.</a></li>
+<li><a href="http://developer.berlios.de/bugs/?func=browse&group_id=2112&set=open">Open Bugs.</a></li>
+</ul>
 
+<h3>Project Wiki:</h3>
+
+<ul>
+<li><a href="http://openfacts.berlios.de/index-en.phtml?title=DeStar">In English</a>.</li>
+<li><a href="http://openfacts.berlios.de/index-es.phtml?title=DeStar">En español</a>.</li>
+<li><a href="http://openfacts.berlios.de/index-de.phtml?title=DeStar">Deutsch</a>.</li>
+</ul>
+
+<h3>Developers mailing list:</h3>
+
+<ul>
+<li>http://lists.berlios.de/mailman/listinfo/destar-dev</li>
+<li>destar-dev@lists.berlios.de</li>
+</ul>
