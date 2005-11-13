f9336fc969be3c9ccfd6a0591c33088b13557264
alerios <alerios@8f11f118-d7e1-0310-9c17-a614ba8dcf8f>
2005-11-13 00:06:38 +0000

    * Added some screenshots

--- web/about.php
+++ web/about.php
@@ -1,8 +1,11 @@
 <h2 id="contentHeading">About</h2>
 
+<p>DeStar comes with ABSOLUTELY NO WARRANTY. This is free software,
+you are welcome to redistribute it under certain conditions;
+see the included files GPL-2.txt and COPYRIGHT.txt</p>
+
 <p>Main development made initially by Holger Schurig, holgerschurig using
 the domain gmx point de</p>
 <p>schurig on #asterisk, #asterisk-dev and #destar at irc.freenode.net</p>
 
 <p>See the <a href="http://developer.berlios.de/project/memberlist.php?group_id=2112">complete member list</a> to view other project members and contributors.</p>
-
--- web/screenshots.php
+++ web/screenshots.php
@@ -1,8 +1,15 @@
 <h2 id="contentHeading">Screenshots</h2>
 
-<p>Sip phone edition</p>
-<p>IAX Trunk edition</p>
-<p>Dialout entries</p>
-<p>Auto-attendant</p>
-<p>Call Detail Records</p>
+<h3>Sip phone edition</h3>
+<a href="http://destar.berlios.de/imgs/screenshots/sip_phone_edit.png"><img src="imgs/screenshots/thumb_sip_phone_edit.png"/></a>
+
+<h3>IAX Trunk edition</h3>
+<a href="http://destar.berlios.de/imgs/screenshots/iax_trunk_edit.png"><img src="imgs/screenshots/thumb_iax_trunk_edit.png"/></a>
+
+<!--<h3>Dialout entries</h3>-->
+
+<h3>Auto-attendant</h3>
+<a href="http://destar.berlios.de/imgs/screenshots/autoattendant_edit.png"><img src="imgs/screenshots/thumb_autoattendant_edit.png"/></a>
+
+<!--<h3>Call Detail Records</h3>-->
 
