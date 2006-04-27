f85043b65599d0d2b541642c35c27ee7a8336b8c
ceronman <ceronman@8f11f118-d7e1-0310-9c17-a614ba8dcf8f>
2006-04-27 20:29:17 +0000

    see changelog

--- trunk/configlets.py
+++ trunk/configlets.py
@@ -526,7 +526,7 @@ class Cfg(Holder):
 					
 	def renameDependencies(self, new_name):
 		for dep in self.dependent_objs:
-				dep.configlet.name = new_name
+				dep.configlet.__dict__[dep.dependency.name] = new_name
 
 	def hasDependencies(self):
 		return len(self.dependent_objs) > 0
