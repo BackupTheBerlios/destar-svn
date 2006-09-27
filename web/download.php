7d8a13c7fc86c48c4dc9488dc00d32c0ba675f98
alerios <alerios@8f11f118-d7e1-0310-9c17-a614ba8dcf8f>
2006-09-27 05:52:56 +0000

    * Updated to reflect 0.2 release.

--- web/download.php
+++ web/download.php
@@ -1,40 +1,28 @@
 <h2 id="contentHeading">Download Area</h2>
 
-<h3>Get Destar v0.2beta, for Asterisk 1.2.X</h3>
+<h3>Destar v0.2.0, for Asterisk 1.2.X</h3>
 
 <h4>Tarball (.tgz) file</h4>
 
 <p>To see the <b>Software Requirements</b> of this release of DeStar, please look at the Installation Instructions that come on the sources.<p>
 
 <ul>
-<li>file: <a href="http://prdownload.berlios.de/destar/destar-0.2beta.tar.bz2">destar-0.2beta.tar.bz2</a> [<a href="https://developer.berlios.de/project/shownotes.php?release_id=10317">release notes</a>] or</li>
+<li>file: <a href="http://prdownload.berlios.de/destar/destar-0.2.0.tar.bz2">destar-0.2.0.tar.bz2</a> [<a href="https://developer.berlios.de/project/shownotes.php?release_id=11175">release notes</a>] or</li>
 
-<li>file: <a href="http://prdownload.berlios.de/destar/destar-0.2beta.tar.gz">destar-0.2beta.tar.gz</a> </li>
-</ul>
-
-<h3>Get Destar v0.1.X, for Asterisk 1.0.X</h3>
-
-<h4>Tarball (.tgz) file</h4>
-
-<p>To see the <b>Software Requirements</b> of DeStar, please look at the 
-<a href="http://openfacts.berlios.de/index-en.phtml?title=DeStarInstall">Installation Instructions</a>.<p>
-
-<ul>
-<li>File: <a href="http://download.berlios.de/destar/destar-0.1.1.tar.gz">destar-0.1.1.tar.gz</a> [<a href="https://developer.berlios.de/project/shownotes.php?release_id=8808">Release Notes</a>] [<i>MD5SUM: e46d27d4523d3d5e0345da24475123b8</i>]</li>
-<li>File: <a href="http://download.berlios.de/destar/destar-0.1.tar.gz">destar-0.1.tar.gz</a> [<a href="https://developer.berlios.de/project/shownotes.php?release_id=7993">Release Notes</a>] [<i>MD5SUM: 70546ea8cbdec12a8520824ebf415f11</i>]</li>
+<li>file: <a href="http://prdownload.berlios.de/destar/destar-0.2.0.tar.gz">destar-0.2.0.tar.gz</a> </li>
 </ul>
 
 <h4>Debian package</h4>
 
 <ul>
-<li>On testing or unstable:  <i>#apt-get install destar python-sqlite python-pychart</i></li>
+<li>On testing or unstable:  <i>#apt-get install destar</i></li>
 <li><a href="http://packages.debian.org/destar">Deb file</a>.</li>
 </ul>
 
-<h4>Latest svn snapshot</h4>
+<h4>Subversion Repository</h4>
 
-<p>SVN copy of the 0.1.X version:</p>
-<p><i>svn checkout svn://svn.berlios.de/destar/tags/0.1</i></p>
+<p>SVN copy of the 0.2.0 version:</p>
+<p><i>svn checkout svn://svn.berlios.de/destar/tags/0.2.0</i></p>
 
 <p>To grab the last svn copy (development version):</p>
 <p><i>svn checkout svn://svn.berlios.de/destar/trunk</i></p>
--- web/home.php
+++ web/home.php
@@ -3,18 +3,23 @@
 <p>Destar is a web-based interface to manage the Asterisk PBX.</p>
 <p>It provides high-level abstraction of the Asterisk configuration.</p>
 
-<p><i>Download the latest version: <b><a href="/index.php?c=2">destar-0.2beta</a></b></i></p>
+<p><i>Download the latest version: <b><a href="/index.php?c=2">destar-0.2.0</a></b></i></p>
 
-<p>
-     Yes, it's true. Almost 6 months have passed since the last release and we have worked a lot to improve <a href="http://destar.berlios.de">Destar</a> and make it more stable and useful.
-</p>
-<p>Basically, this beta release contains:<br /> <ul style="font-family: verdana;">   <li>A new interface style.   </li>   <li>Referencial integrity.   </li>   <li>Better i18n support.   </li>   <li>Better statistics for CDR.</li>   <li>Use of some <a href="http://www.asterisk.org">asterisk</a> 1.2.X new features.<br />  </li>   <li>Many new features.   </li>   <li>Several Bug fixes.</li> </ul> <span style="font-family: verdana;">We hope you can enjoy testing it and helping us release the 0.2 version. </span><br />  <br /> <span style="font-family: verdana;"> You can find it at:</span><br />  <a style="font-family: verdana;" href="https://developer.berlios.de/project/showfiles.php?group_id=2112">https://developer.berlios.de/project/showfiles.php?group_id=2112</a><br />  <br /> <span style="font-family: verdana;"> And file the bugs you find on:</span><br />  <a style="font-family: verdana;" href="https://developer.berlios.de/bugs/index.php">https://developer.berlios.de/bugs/index.php</a>
-
-    </p>
-
+<p>This new version contains many improvements to the 0.1 one:<br /> 
+<ul style="font-family: verdana;">   
+<li>A new interface style (see screenshots).   </li>   
+<li>Referencial integrity.   </li>   
+<li>Better i18n support (english, spanish and french right now).   </li>   
+<li>Better statistics for CDR.</li>   
+<li>Updated for <a href="http://www.asterisk.org">asterisk</a> 1.2.X.<br />  </li>   
+<li>Many new features.   </li>   
+<li>Several Bug fixes.</li>
+<li>First version of user manual</li> 
+</ul> 
 
 <p>Destar main features include:</p>
 <ul>
+<li>Hosted PBX - virtual PBX features.</li>
 <li>Extensions management: SIP, IAX, Zap, and more.</li>
 <li>Auto-attendants support.</li>
 <li>Trunks management: SIP, IAX, Zap, ZapPRI, and more.</li>
@@ -22,9 +27,10 @@
 <li><a href="http://www.asternic.org">Asternic Flash Operator Panel</a> integration.</li>
 <li>Call Detail Records search and graphical reports.</li>
 <li>Many application applets incluided: Voice Mail, Meeting Room, and more.</li>
+<li>Extensible pluguin-based architecture.</li>
 </ul>
 
-<img src="imgs/destar-0.1.png" alt="Destar small screenshot" />
+<img src="imgs/destar-0.2.0.png" alt="Destar small screenshot" />
 
 
 <p>NOTE: The Asterisk trademark referred to within this site is the sole property of Digium, Inc.</p>
