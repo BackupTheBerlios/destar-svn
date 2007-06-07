53f11aec7822e5a998c6d01d07911e126ddb0691
santiago <santiago@8f11f118-d7e1-0310-9c17-a614ba8dcf8f>
2007-06-07 22:45:29 +0000

    Added port setting in sip.conf

--- branches/0.2.X/cfg_trunk_sip.py
+++ branches/0.2.X/cfg_trunk_sip.py
@@ -192,6 +192,8 @@ class CfgTrunkSiptrunk(CfgTrunk):
 			c.append("fromuser=%s" % self.id)
 			c.append("secret=%s" % self.pw)
 			c.append("host=%s" % self.host)
+			if self.port:
+				c.append("port=%s" % self.port)
 			c.append("context=in-%s" % self.name)
 			c.append("canreinvite=no")
 			if self.fromdomain:
